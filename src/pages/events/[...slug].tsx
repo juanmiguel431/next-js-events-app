9984785a09c7c7c524bbc9ad7ceefe6ed68f806f
juanmiguel431 <juanmiguel431@gmail.com>
2023-07-03 22:45:50 -0400

    ResultsTitle component added.

--- src/pages/events/[...slug].tsx
+++ src/pages/events/[...slug].tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "@/models/dummy-data";
 import EventList from "@/components/events/event-list";
+import ResultsTitle from "@/components/results-title/results-title";
 
 const FilteredEventsPage: React.FC = () => {
   const router = useRouter();
@@ -25,8 +26,11 @@ const FilteredEventsPage: React.FC = () => {
     return <p className="center">No events found.</p>
   }
 
+  const date = new Date(year, month - 1);
+
   return (
     <div className="filtered-events-page">
+      <ResultsTitle date={date} />
       <EventList items={events} />
     </div>
   );
