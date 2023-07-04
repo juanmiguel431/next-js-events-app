044240e19a0c951903825b4e39e4f87d1191dbcd
juanmiguel431 <juanmiguel431@gmail.com>
2023-07-03 21:56:03 -0400

    EventsSearch component added.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,18 +1,21 @@
-import React, { PropsWithChildren } from "react";
+import React, { MouseEventHandler, PropsWithChildren } from "react";
 import Link from "next/link";
 import classes from "./button.module.css";
 import { UrlObject } from "url";
 
 interface ButtonProps extends PropsWithChildren {
-  href: UrlObject | string;
+  href?: UrlObject | string;
+  onClick?: MouseEventHandler
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
 
+  if (props.href === undefined) {
+    return <button className={classes.btn} onClick={props.onClick} >{props.children}</button>;
+  }
+
   return (
-    <div className="button">
-      <Link href={props.href} className={classes.btn} >{props.children}</Link>
-    </div>
+    <Link href={props.href} className={classes.btn} >{props.children}</Link>
   );
 }
 
--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import EventList from "@/components/events/event-list";
 import { getAllEvents } from "@/models/dummy-data";
+import EventSearch from "@/components/events/event-search";
 
 const EventsPage: React.FC = () => {
   const allEvents = getAllEvents();
 
   return (
     <div className="events-page">
+      <EventSearch />
       <EventList items={allEvents} />
     </div>
   );
