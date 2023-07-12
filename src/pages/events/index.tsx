2bd34efe104d9d3d332b3fda2abf0e70f890a774
juanmiguel431 <juanmiguel431@gmail.com>
2023-07-12 18:07:48 -0400

    Working on All Events Page.

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import EventList from "@/components/events/event-list";
-import { getAllEvents } from "@/models/dummy-data";
+import { getAllEvents } from '@/helpers/api-utils';
 import EventSearch from "@/components/events/event-search";
 import { useRouter } from "next/router";
+import { GetStaticProps, NextPage } from 'next';
+import { Event } from '@/models';
 
-const EventsPage: React.FC = () => {
-  const allEvents = getAllEvents();
+interface EventsPageProps {
+  events: Array<Event>;
+}
+
+const EventsPage: NextPage<EventsPageProps> = ({ events }) => {
   const router = useRouter();
 
   const onSearch = (year: number, month: number) => {
@@ -20,9 +25,18 @@ const EventsPage: React.FC = () => {
   return (
     <div className="events-page">
       <EventSearch onSearch={onSearch}/>
-      <EventList items={allEvents}/>
+      <EventList items={events}/>
     </div>
   );
 }
 
 export default EventsPage;
+
+
+export const getStaticProps: GetStaticProps<EventsPageProps> = async () => {
+  const events =  await getAllEvents();
+
+  return {
+    props: { events }
+  };
+}
