478b16ce9bd7354bcc184ce4659e0b26eb2a2150
juanmiguel431 <juanmiguel431@gmail.com>
2023-07-12 18:24:31 -0400

    FilteredEventsPage using Firebase

--- src/pages/events/browse/[...slug].tsx
+++ src/pages/events/browse/[...slug].tsx
@@ -1,41 +1,27 @@
-import React from "react";
-import { useRouter } from "next/router";
-import { getFilteredEvents } from "@/models/dummy-data";
-import EventList from "@/components/events/event-list";
-import ResultsTitle from "@/components/results-title/results-title";
-import Button from "@/components/ui/button";
-import ErrorAlert from "@/components/ui/error-alert/error-alert";
+import React from 'react';
+import { useRouter } from 'next/router';
+import EventList from '@/components/events/event-list';
+import ResultsTitle from '@/components/results-title/results-title';
+import Button from '@/components/ui/button';
+import { GetServerSideProps, NextPage } from 'next';
+import { Event } from '@/models';
+import { ParsedUrlQuery } from 'querystring';
+import { getFilteredEvents } from '@/helpers/api-utils';
 
-const FilteredEventsPage: React.FC = () => {
+interface FilteredEventsPageProps {
+  events: Event[];
+  year: number;
+  month: number;
+}
+
+const FilteredEventsPage: NextPage<FilteredEventsPageProps> = ({ events, year, month }) => {
   const router = useRouter();
-
-  const data = router.query.slug;
-
-  if (!data) {
-    return <p className="center">Loading...</p>
-  }
-
-  const year = +data[0];
-  const month = +data[1];
-
-  if (isNaN(year) || isNaN(month) || year < 1900 || month < 1 || month > 12) {
-    return <>
-      <ErrorAlert>
-        <p className="center">Invalid filters. Please, adjust the values.</p>
-      </ErrorAlert>
-      <div className="center">
-        <Button href='/events'>Show all events</Button>
-      </div>
-    </>
-  }
-
-  const events = getFilteredEvents({ year, month });
 
   if (events.length === 0) {
     return <>
       <p className="center">No events found for the chosen filters.</p>
       <div className="center">
-        <Button href='/events'>Show all events</Button>
+        <Button href="/events">Show all events</Button>
       </div>
     </>;
   }
@@ -44,10 +30,44 @@ const FilteredEventsPage: React.FC = () => {
 
   return (
     <div className="filtered-events-page">
-      <ResultsTitle date={date} />
-      <EventList items={events} />
+      <ResultsTitle date={date}/>
+      <EventList items={events}/>
     </div>
   );
 }
 
 export default FilteredEventsPage;
+
+
+interface Params extends ParsedUrlQuery {
+  slug: string[]
+}
+
+export const getServerSideProps: GetServerSideProps<FilteredEventsPageProps> = async (context) => {
+  const data = context.query.slug;
+
+  if (!data) {
+    return {
+      notFound: true
+    };
+  }
+
+  const year = +data[0];
+  const month = +data[1];
+
+  if (isNaN(year) || isNaN(month) || year < 1900 || month < 1 || month > 12) {
+    return {
+      notFound: true
+    };
+  }
+
+  const events = await getFilteredEvents({ year, month });
+
+  return {
+    props: {
+      events: events,
+      year: year,
+      month: month
+    }
+  }
+};
