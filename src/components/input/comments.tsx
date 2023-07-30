2b2f7a8d20f040f5b005251dd29b732a05d59420
juanmiguel431 <juanmiguel431@gmail.com>
2023-07-30 00:13:07 -0400

    loading text added for comments.

--- src/components/input/comments.tsx
+++ src/components/input/comments.tsx
@@ -10,13 +10,17 @@ function Comments(props: any) {
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState<Comment[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
 
   useEffect(() => {
     if (showComments) {
       const fetchComments = async () => {
+        setIsLoading(true);
         const response = await fetch(`/api/comments/${eventId}`);
         const data = await response.json();
         setComments(data.comments)
+        setIsLoading(false);
       }
 
       fetchComments();
@@ -28,27 +32,27 @@ function Comments(props: any) {
   }
 
   async function addCommentHandler(commentData: any) {
-    // send data to API
-    const response = await fetch(`/api/comments/${eventId}`, {
+    setIsSaving(true);
+    await fetch(`/api/comments/${eventId}`, {
       method: 'POST',
       body: JSON.stringify(commentData),
       headers: {
         'Content-Type': 'application/json'
       }
     });
-
-    const data = await response.json();
-    console.log(data);
+    setIsSaving(false);
   }
 
   return (
     <section className={classes.comments}>
-      <button onClick={toggleCommentsHandler}>
+      <button disabled={isLoading} onClick={toggleCommentsHandler}>
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments &&
           <>
               <NewComment onAddComment={addCommentHandler}/>
+            {isSaving && <p>Saving a new comment!</p>}
+            {isLoading && <p>Loading list of comments!</p>}
               <CommentList comments={comments}/>
           </>
       }
