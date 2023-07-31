4d8cdf0af8e6c6a424c87dbbbd24df4cd23403db
juanmiguel431 <juanmiguel431@gmail.com>
2023-07-30 23:08:07 -0400

    NotificationFooter was simplified.

--- src/components/ui/notification/notification-footer.tsx
+++ src/components/ui/notification/notification-footer.tsx
@@ -6,24 +6,8 @@ import NotificationContext from '@/store/notification-context';
 
 const NotificationFooter: React.FC<NotificationFooterProps> = (props) => {
   const notificationCtx = useContext(NotificationContext);
-
   const { title, message, status } = props;
-
-  let statusClasses = '';
-
-  if (status === 'success') {
-    statusClasses = classes.success;
-  }
-
-  if (status === 'error') {
-    statusClasses = classes.error;
-  }
-
-  if (status === 'pending') {
-    statusClasses = classes.pending;
-  }
-
-  const activeClasses = `${classes.notification} ${statusClasses}`;
+  const activeClasses = `${classes.notification} ${classes[status]}`;
 
   return (
     <div
